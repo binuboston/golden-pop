a816618379003084b5f6de0cc55111b82f60fec4
binuboston <34465688+binuboston@users.noreply.github.com>
2025-04-27 03:48:53 +0000

    product card styling

--- js/main.js
+++ js/main.js
@@ -76,4 +76,12 @@ function closeVideo() {
   player.src = "";
   modal.style.display = "none";
 }
-
+//header scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) { // when scroll > 50px
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
